688d43fd6863852756eba2cecde5b9b9466ac0bc
Arthur-Ruas <arthurruasafe@gmail.com>
2024-02-22 22:13:49 -0300

    Arthur: Inicio da Estilização

---
+++ pw3-atv-arthur/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CardBook from './components/CardBook/CardBook';
+
+function App() {
+  return (
+    <div className="App">
+      <CardBook/>
+    </div>
+  );
+}
+
+export default App;
